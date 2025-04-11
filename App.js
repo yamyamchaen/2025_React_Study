30fb2ceb34ea1f4464da81b8830a7c9dce7ebab6
yamyamchaen <163889145+yamyamchaen@users.noreply.github.com>
2025-04-11 19:48:24 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import Introduce from './Introduce';
+import './App.css';
 
 function App() {
-    const containerStyle = {
-        border : '1px solid black',
-        padding : '20px',
-        width : '160px',
-        height : '190px',
-        display : 'flex',
-        justifyContent : 'center',
-        alignItems : 'center'
-
-    }
-
-    const style = {
-        display : 'flex',
-        justifyContent : 'center',
-        alignItems : 'center',
-        height : '100vh',
-        flexDirection : 'column'
-
-    }
-    return (
-        <div style={style}>
-            <h1>자기소개 카드</h1>
-            <div style={containerStyle}>
-                <Introduce name='정채은' age='23' hobby='포켓몬' />
-            </div>
-        </div>
-    );
+  return (
+      <div className="app-wrapper">
+        <h1 className="title">✨ 자기소개 카드 ✨</h1>
+        <Introduce name="정채은" age="23" hobby="포켓몬" />
+      </div>
+  );
 }
 
 export default App;
